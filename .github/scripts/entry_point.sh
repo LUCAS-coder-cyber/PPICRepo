cfe669a21698e85a36bf96b66903eada4018605c
LUCAS-coder-cyber <lucas.millican@student.uts.edu.au>
2026-05-23 11:40:57 +1000

    Project automation

--- .github/scripts/entry_point.sh
+++ .github/scripts/entry_point.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-echo "Starting Frequency Analyzer..."
+echo "------------------------"
+echo "Executing todo.py"
+echo "------------------------"
+python3 /app/.github/scripts/todo.py | tee /app/todo_output.txt
 
-# Running the Python frequency analyzer
-FREQ_RESULT=$(python3 /app/.github/scripts/frequency.py /app/data.txt)
+echo "------------------------"
+echo "Executing todo-test.py"
+echo "------------------------"
+python3 /app/.github/scripts/todo-test.py | tee /app/test_output.txt
 
-# Running update_readme.sh
-bash /app/.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
+echo "------------------------"
+echo "Executing update_index.sh"
+echo "------------------------"
+bash /app/.github/scripts/update_index.sh /app/todo_output.txt /app/test_output.txt
 
-echo "Process Completed!"
+echo "------------------------"
+echo "Done!"
+echo "------------------------"
